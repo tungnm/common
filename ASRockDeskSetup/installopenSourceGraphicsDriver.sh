dfc4543352c1c00b279c7c8b750817ccfb056274
Tung <mtung09@gmail.com>
2014-01-10 02:43:42 -0800

    Add script to install opensource graphics

---
+++ ASRockDeskSetup/installopenSourceGraphicsDriver.sh
@@ -0,0 +1,6 @@
+sudo apt-get remove --purge fglrx fglrx-amdcccle fglrx-dev
+sudo apt-get remove --purge xserver-xorg-video-ati xserver-xorg-video-radeon
+sudo apt-get install xserver-xorg-video-ati
+sudo apt-get install --reinstall libgl1-mesa-glx libgl1-mesa-dri xserver-xorg-core
+sudo mv /etc/X11/xorg.conf /etc/X11/xorg.conf.backup
+sudo rm -rf /etc/ati
