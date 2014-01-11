d16bbbf893b405e72d71052b71821f94a11adc5a
Tung Nguyen <mtung09@gmail.com>
2014-01-11 13:46:18 -0800

    Add git , zsh install and fix mouseSpeedReduce.sh

--- ASRockDeskSetup/mouseSpeedReduce.sh
+++ ASRockDeskSetup/mouseSpeedReduce.sh
@@ -1,9 +1,9 @@
 #written by Tung - May 12
 #look for mouse id or name:
 #xinput --list
-
+echo "Look for id of the mouse"
 #setting mouse sensitivity:
-id=$(xinput --list|grep Razer|sed "s/.*id=//"|sed "s/\t.*//")
+id=$(xinput --list|grep DeathAdder|sed "s/.*id=//"|sed "s/\t.*//")
 xinput --list
 echo "the id of Razor mouse is: $id"
 echo "xinput --set-prop $id \"Device Accel Constant Deceleration\" 4" >>~/.zshrc
--- setup.sh
+++ setup.sh
@@ -1,8 +1,17 @@
+echo "setup vim setting"
 cp .vimrc ~/.vimrc
 cp .zshrc ~/.zshrc
 cp -r .vim ~/
+echo "install Git"
+sudo apt-get install git
+echo "configure git global"
+git config --global color.ui auto
+git config --global user.name "Tung Nguyen"
+git config --global user.email mtung09@gmail.com
 echo "installing command-not-found for zsh..."
 sudo apt-get install command-not-found
+echo "install zsh..."
+sudo apt-get install zsh
 echo "trying to change to zsh..."
 chsh -s /usr/bin/zsh
 echo "If no error found above, please restart to use zsh"
