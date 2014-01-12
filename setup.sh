bbb2850afc76f9fc1ff3790c9c545667c17377e4
Tung Nguyen <mtung09@gmail.com>
2014-01-11 17:40:51 -0800

    fixed setup script to look for zsh folder correctly

--- setup.sh
+++ setup.sh
@@ -2,21 +2,23 @@ echo "setup vim setting"
 cp .vimrc ~/.vimrc
 cp .zshrc ~/.zshrc
 cp -r .vim ~/
-echo "install Git"
-sudo apt-get install git
+
 echo "configure git global"
 git config --global color.ui auto
-git config --global user.name "Tung Nguyen"
-git config --global user.email mtung09@gmail.com
-echo "installing command-not-found for zsh..."
-sudo apt-get install command-not-found
-echo "install zsh..."
-sudo apt-get install zsh
-echo "trying to change to zsh..."
-chsh -s /usr/bin/zsh
-echo "If no error found above, please restart to use zsh"
-
 git config --global core.editor vim
 git config --global merge.tool vimdiff
-chsh -s /bin/zsh
-echo "changed to zsh shell, please log out and log in again"
+
+echo "installing command-not-found for zsh..."
+sudo apt-get install command-not-found
+
+#store command output in a variable:
+zshBin=$(which zsh)
+# -z varName which check if the string varName is empty
+if [ -z zshBin ]; then
+	echo " zsh not found, install zsh..."
+	sudo apt-get install zsh
+fi
+
+echo "trying to change to zsh at $zshBin..."
+chsh -s $zshBin 
+echo "If no error found above, please log out and relogin to use zsh"
