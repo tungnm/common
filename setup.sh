d0ad2e8d1bb693610dc58995b29421a758a04bff
Tung Nguyen <mtung09@gmail.com>
2014-03-24 03:35:28 +0000

    Fixed bash if script to install vim and zsh if neccesary

--- setup.sh
+++ setup.sh
@@ -11,12 +11,25 @@ git config --global merge.tool vimdiff
 echo "installing command-not-found for zsh..."
 sudo apt-get install command-not-found
 
+vimDir=$(which vim)
+# note that the space is  important, shoudl follow the space strictly as in here
+if [ -e $vimDir ] 
+then
+	echo "vim found at: $vimDir"
+else
+	echo " vim not found, install vim"
+	sudo apt-get install vim 
+fi
+
 #store command output in a variable:
 zshBin=$(which zsh)
-# -z varName which check if the string varName is empty
-if [ -z zshBin ]; then
+# -e varName: check if the file exists  
+if [ -e $zshBin ] 
+then
+	echo "zsh found at:$zshBin" 
+else
 	echo " zsh not found, install zsh..."
-	sudo apt-get install zsh
+	sudo apt-get install zsh 
 fi
 
 echo "trying to change to zsh at $zshBin..."
